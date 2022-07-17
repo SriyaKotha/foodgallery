43b5f1ee78be8b1d4e858a2c774ae80eaee1252c
190030874_Sriya Kotha <190030874@kluniversity.in>
2022-07-17 13:45:23 +0530

    5 Commit

--- CRUD/admin.py
+++ CRUD/admin.py
@@ -3,11 +3,11 @@ from .models import Image
 from django.utils.safestring import mark_safe
 from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
-
 class ImageAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    fields = ('id', 'ImgName', 'image', 'ImgDetails')
-    list_display = ('id', 'ImgName', 'image', 'ImgDetails')    
+    fields = ('id', 'ImgName', 'ImgDetails')
+    list_display = ('id', 'ImgName', 'ImgDetails')
+    search_fields =['ImgName']
+    ordering = ['id']
 
 admin.site.register(Image, ImageAdmin)
 
