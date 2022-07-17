ebccdcd2216b83c9910b296f5b42b1d967e948a7
190030874_Sriya Kotha <190030874@kluniversity.in>
2022-07-17 13:33:34 +0530

    3 Commit

--- CRUD/admin.py
+++ CRUD/admin.py
@@ -1,22 +1,13 @@
 from django.contrib import admin
 from .models import Image
 from django.utils.safestring import mark_safe
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
-class ImageAdmin(admin.ModelAdmin):
+class ImageAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     fields = ('id', 'ImgName', 'image', 'ImgDetails')
-    list_display = ('id', 'ImgName', 'image', 'ImgDetails')
-    search_fields =['ImgName']
-    ordering = ['id']
-    readonly_fields = ['image']
-
-    def image(self, obj):
-        return mark_safe('<img src="{url}" width="100" height="100"/>'.format(
-            url = obj.ImgURL,
-            )
-    )
-
+    list_display = ('id', 'ImgName', 'image', 'ImgDetails')    
 
 admin.site.register(Image, ImageAdmin)
 
